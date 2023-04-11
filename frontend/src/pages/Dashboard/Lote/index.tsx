0b1f4d6b710c4612e5fa98864b0abefe2440e081
Deyvid William <deyvidwms@gmail.com>
2023-04-11 17:29:39 -0300

    feat: esqueleto do projeto

---
+++ frontend/src/pages/Dashboard/Lote/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Lote: React.FC = () => {
+  return (
+    <h1>Lote</h1>
+  );
+}
+
+export default Lote;
---
+++ frontend/src/pages/Dashboard/Produto/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Produto: React.FC = () => {
+  return (
+    <h1>Produto</h1>
+  );
+}
+
+export default Produto;
