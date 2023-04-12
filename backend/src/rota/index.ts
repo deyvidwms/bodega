a7e2ec91529e236588c5cb067c517d6a798476de
Nathãn Vitor de Lima <nathan2001lima@gmail.com>
2023-04-12 19:34:09 -0300

    fix: adiciona rota de bodega

--- backend/src/rota/index.ts
+++ backend/src/rota/index.ts
@@ -4,9 +4,11 @@ import loteRota from "./loteRota";
 import produtoRota from "./produtoRota";
 import usuarioRota from "./usuarioRota";
 import vendaRota from "./vendaRota";
+import bodegaRota from "./bodegaRota";
 
 const rota = Router();
 
+rota.use('/bodega', bodegaRota);
 rota.use('/categoria-produto', categoriaProdutoRota);
 rota.use('/lote', loteRota);
 rota.use('/produto', produtoRota);
