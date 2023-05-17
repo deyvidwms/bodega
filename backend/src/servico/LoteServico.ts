1327d0aba42314e40705defd6e49b0d09e9125dd
Deyvid William <deyvidwms@gmail.com>
2023-05-17 19:20:56 -0300

    update servico lote

--- backend/src/servico/LoteServico.ts
+++ backend/src/servico/LoteServico.ts
@@ -56,17 +56,11 @@ export default class LoteServico implements ServicoEscrita<Lote> {
   }
 
   async criar(lote: Lote): Promise<Lote> {
-    lote.validade = new Date('2024-10-26T00:00:00.000Z');
-    lote.compradoEm = new Date('2024-10-26T00:00:00.000Z');
-    lote.emPromocao = false;
     await this.validarCadastro(lote);
     return await LoteServico.repositorio.criar(lote);
   }
 
   async atualizar(lote: Lote): Promise<Lote> {
-    lote.validade = new Date('2024-10-26T00:00:00.000Z');
-    lote.compradoEm = new Date('2024-10-26T00:00:00.000Z');
-    lote.emPromocao = false;
     await this.validarAtualizacao(lote);
     return await LoteServico.repositorio.atualizar(lote);
   }
